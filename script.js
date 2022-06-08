4b00beadec357bb2a6c964b791881ffd5dac54fa
Umangcodesnow <umangcodes@gmail.com>
2022-06-08 18:27:31 +0530

    Updating Js

--- script.js
+++ script.js
@@ -1,1 +1,197 @@
-window.alert("I Really Love Success");
+//window.alert("I Really Love Success");
+//gives an alert on windows by  button
+
+// let fName = "Umang";
+// let age = 21;
+// let student = true;
+
+
+// console.log("My name is" , fName ,"Gupta");
+// console.log("And I'm ",age, "years old");
+// console.log("am i a student",student);
+
+// //if I want to change the innerhtml element
+// document.getElementById("rat").innerHTML ="Hello " + fName;
+// document.getElementById("p2").innerHTML = age + "And I also love Success";
+
+//How to accept user  input
+//easy way
+ 
+//let userName = window.prompt("What's your username?");
+//console.log(userName); // Easy method
+//difficult method
+
+
+//first we create html 16-18 then
+// let userName;
+
+// document.getElementById("mybutton").onclick = function() {
+//     userName = document.getElementById("myText").value;
+//     document.getElementById("myLabel").innerHTML = "Hello " + userName;
+// }
+
+//type conversion: converting one data type tp nother
+
+// let age = window.prompt("How old are You?");
+
+// age = Number(age); //converting age into a number
+// age += 1; //by default this age is string 1
+
+// console.log("Happy Birthday! You Are", age,"Years old");
+
+// let x;
+// let y;
+// let z;
+
+// // x = 39; //if we were to convert a number then 
+// // y = 3.14; // /if we were to convert a number then
+// // z =  "";  //if we were to convert an empty string then
+
+// x = Number(39);
+// y = String(3.14);
+// z = Boolean("") // empty will retuen false and something written will give true
+
+
+
+// //to check its types
+// console.log(x, typeof x);
+// console.log(y,typeof y);
+// console.log(z,typeof z);
+
+//Use of const where we do not want to change our value
+//const Var are always Caitalised
+// const PI = 3.14;
+// let radius;
+// let circumference;
+
+// radius = window.prompt("Enter the radius");
+// radius= Number(radius);
+
+// circumference = 2 * PI * radius;
+
+// console.log("The circumference is:", circumference);
+// let a;
+// let x = 3.14566;
+// let y = 5;
+// let k = 4;
+// let maximum ;
+// let minimum ;
+
+
+// x = Math.round(x); //round it off
+// x = Math.floor(x); //removes decimal
+// x = Math.ceil(x);
+// x = Math.pow(x ,2);
+// x = Math.sqrt(x);
+// x = Math.abs(x);
+// maximum = Math.max(x,y,k);
+// minimum = Math.min(x,y,k);
+
+// a = Math.PI;
+// console.log(x);
+
+//Find the hypotenuse   
+//00:38:58
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
